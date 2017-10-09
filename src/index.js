2fac60b1b8679967c5b0f0e455d2305f8a1e5ea1
Elia Shkedy <ELIAS@il.ibm.com>
2017-10-09 19:05:31 +0300

    WIP props and maps

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCQ5u5CSDlUtL9wfWtYbbRRgz4gsgPPNgQ';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videosList: [] };
+
+        // call for youtube API with: key, what to search and the callback function to store the response
+        YTSearch( {key: API_KEY, term: "sufrboards"}, 
+            // the arrow function is the callback functiojn
+            // (returnedVidList) => {
+                //this.setState( {videos: returnedVidList} );
+            // key name and value variable are can be the same and use the es6 syntax -
+            // see: https://www.udemy.com/react-redux/learn/v4/t/lecture/4288076?start=0 
+            // at: 6:20
+            // same as: this.setState({videosList : videosList});
+            (videosList) => {
+                this.setState({videosList});
+            }
+        );
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videosList = {this.state.videosList} />
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, 
+                document.querySelector('.container'))
